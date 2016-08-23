c94a87c3793a66f51e5998e198fadf3af644720b
Sunmin Lee <sunm.lee@samsung.com>
2016-08-23 17:17:54 +0900

    Upgrade: Back rpm db up for Tizen 3.0
    Because rpm db is in RW partition (/opt/var/lib/rpm),
    in the upgrade operation rpm db should be changed as
    2.4 -> 3.0.
    To do this, when create the upgrade image the rpm db should
    be backed up and moved to RW path of 2.4 in the upgrade process.
    
    Change-Id: If940abe0132dffcaaa0bc791ceecd3662946d68c
    Signed-off-by: Sunmin Lee <sunm.lee@samsung.com>

--- upgrade/update-image.sh
+++ upgrade/update-image.sh
@@ -3,5 +3,9 @@
 # Modify normal image to upgrade image
 #
 
+# Back rpm db up for Tizen 3.0
+mkdir /system-update/data/rpm
+cp -arf /var/lib/rpm/* /system-update/data/rpm
+
 # remove RW partitions' files
 rm -rf /opt/*
