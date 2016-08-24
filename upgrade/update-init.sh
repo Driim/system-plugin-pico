7d55bce57285b8a88d27a0a0b36ba4524fa0c44f
Sunmin Lee <sunm.lee@samsung.com>
2016-08-24 13:41:41 +0900

    Upgrade: Mount user partition in upgrade mode
    The lazy mount makes user partition mounted
    separately. For RW update, user partition also should
    be mounted. This is done in the start of upgrade.
    
    Change-Id: I3db70f3bb698d22371747530c545145836b57e09
    Signed-off-by: Sunmin Lee <sunm.lee@samsung.com>

--- upgrade/update-init.sh
+++ upgrade/update-init.sh
@@ -5,6 +5,17 @@
 PATH=/bin:/usr/bin:/sbin:/usr/sbin
 OWNER_HOME=/opt/usr/home/owner
 
+USER_DISK=`ls /dev/disk/by-partlabel/ | grep -i user`
+USER_MNT=/opt/usr
+
+# Mount user partition (specific to lazy mount)
+mount | grep "$USER_MNT" > /dev/null
+
+if [ $? != "0" ]
+then
+	/usr/bin/mount PARTLABEL=$USER_DISK $USER_MNT
+fi
+
 # Create home directory
 test ! -e /opt/usr/home && mkdir -p /opt/usr/home
 
